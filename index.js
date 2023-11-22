b6436290980b1545e1fbba84ec74d738028e7f08
Junu <housemusic264@gmail.com>
2023-11-21 23:31:46 -0500

    initial committ

---
+++ index.js
@@ -0,0 +1,20 @@
+//Objectives
+//Create variable declarations using both let and const.
+//Apply different types of data literals.
+//Use arithmetic operators to manipulate data.
+//Use comparison operators to compare data.
+//Perform string concatenations.
+//Implement escape sequences in strings for special characters.
+//Use template literals for string interpolation and multi-line strings.
+//Create effective documentation through the use of comments.
+
+
+let x = 5;
+let y = 10;
+
+let sum = x + y;
+let difference = x - y;
+let product = x * y;
+let quotient = x / y;
+let remainder = x % y;
+console.log(x+y);
