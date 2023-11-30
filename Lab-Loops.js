2edcf0c0720603237bd072073daefaf4dcc64929
Junu <housemusic264@gmail.com>
2023-11-29 19:11:07 -0500

    Prime Time

--- Lab-Loops.js
+++ Lab-Loops.js
@@ -14,3 +14,31 @@ for (let i = 1; i <= 100; i++) {
 
 
 // Prime Number
+function isPrime(number) {
+    if (number <= 1) {
+        return false; // 1 is not a prime number
+    }
+
+    for (let i = 2; i < number; i++) {
+        if (number % i === 0) {
+            return false; // Found a number that divides evenly, not prime
+        }
+    }
+
+    return true; // It's a prime number!
+}
+
+function findNextPrime(n) {
+    while (true) {
+        if (isPrime(n)) {
+            console.log("Next prime number is:", n);
+            break; // Found the next prime, exit the loop
+        }
+        n++; // Move to the next number and check again
+    }
+}
+
+// Test with different values of n
+findNextPrime(4);
+findNextPrime(5);
+findNextPrime(9);
