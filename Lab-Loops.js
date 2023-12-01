f577d1f54b8e4edace66ec27e219beea910a2f73
Junu <housemusic264@gmail.com>
2023-11-30 21:16:45 -0500

    inital commit parseCVS

--- Lab-Loops.js
+++ Lab-Loops.js
@@ -46,3 +46,21 @@ findNextPrime(9);
 
 // #Part 3 Feeling Loopy
 
+const csvData = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+
+function parseCSV(csvString) {
+    // Split the CSV string into rows using the newline character "\n"
+    const rows = csvString.split('\n');
+
+    // Loop through each row
+    for (const row of rows) {
+        // Split the row into cells using the comma ","
+        const cells = row.split(',');
+
+        // Log each cell in the row
+        console.log(cells[0], cells[1], cells[2], cells[3]);
+    }
+}
+
+// Test with the provided CSV data
+parseCSV(csvData);
---
+++ functions.js
@@ -0,0 +1,40 @@
+/* Practice Activity 1: Write a Function Declaration
+Write a function named computeArea using the function declaration approach.
+It will have two parameters: width & height.
+It will compute the area of a rectangle (width multiplied by height) and return a string in the following form:
+
+The area of a rectangle with a width of _ and a height of _ is ___ square units.
+Invoke the function to test it. */
+
+
+function computeArea(width,height){
+    const area = width * height;
+    return `${width} and ${height} is ${area}`;
+}
+const result = computeArea(4,5);
+console.log(result);
+
+// function computeArea(width, height) {
+//     const area = width * height;
+//     return `The area of a rectangle with a width of ${width} and a height of ${height} is ${area} square units.`;
+// }
+
+// // Example usage:
+// const result = computeArea(5, 8);
+// console.log(result);
+
+/*    Practice Activity 2: Write a Function Expression
+    Write a function named planetHasWater using the function expression syntax.
+
+    It will have one parameter: planet.
+    Return true if the planet argument is either "Earth" or "Mars", otherwise return false.
+    Bonus points if you ensure the function will work regardless of the casing of the planet being passed in ('earth', 'MARS', etc.).
+    Invoke the function a couple of times to test it! */
+
+function planetHasWater(planet){
+    if(){
+
+    }else{
+        
+    }
+}
